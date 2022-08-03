02340a126c4f8de43320773856143fbfa834a751
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2022-08-03 15:30:39 +0300

    Add new icons

--- src/index.js
+++ src/index.js
@@ -1,4 +1,33 @@
 import displayController from './controllers/displayController';
 import './main.css';
 
+// Import images/icons so they can be copied in /dist folder and loaded dynamically when needed
+import clearDay from './assets/imgs/clearDay.jpg';
+import cloudyDay from './assets/imgs/cloudyDay.jpg';
+import rainyDay from './assets/imgs/rainyDay.jpg';
+import snowyDay from './assets/imgs/snowyDay.jpg';
+import clearNight from './assets/imgs/clearNight.jpg';
+import cloudyNight from './assets/imgs/cloudyNight.jpg';
+import rainyNight from './assets/imgs/rainyNight.jpg';
+import snowyNight from './assets/imgs/snowyNight.jpg';
+
+import icon1Day from './assets/icons/01d.png';
+import icon1Night from './assets/icons/01n.png';
+import icon2Day from './assets/icons/02d.png';
+import icon2Night from './assets/icons/02n.png';
+import icon3Day from './assets/icons/03d.png';
+import icon3Night from './assets/icons/03n.png';
+import icon4Day from './assets/icons/04d.png';
+import icon4Night from './assets/icons/04n.png';
+import icon9Day from './assets/icons/09d.png';
+import icon9Night from './assets/icons/09n.png';
+import icon10Day from './assets/icons/10d.png';
+import icon10Night from './assets/icons/10n.png';
+import icon11Day from './assets/icons/11d.png';
+import icon11Night from './assets/icons/11n.png';
+import icon13Day from './assets/icons/13d.png';
+import icon13Night from './assets/icons/13n.png';
+import icon50Day from './assets/icons/50d.png';
+import icon50Night from './assets/icons/50n.png';
+
 displayController.init();
--- webpack.common.js
+++ webpack.common.js
@@ -15,7 +15,7 @@ module.exports = {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
         generator: {
-          filename: './assets/imgs/[name].[hash][ext]',
+          filename: './assets/imgs/[name][ext]',
         },
       },
     ],
